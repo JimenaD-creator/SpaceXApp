192f1b5199c09bc232bbcf6951552d5d7b08fb34
JimenaD-creator <a01403295@tec.mx>
2026-01-27 23:23:17 -0600

    feat: add rocket and launchpad endpoints to SpaceXApi

--- app/src/main/java/com/example/spacexapp/data/remote/SpaceXApi.kt
+++ app/src/main/java/com/example/spacexapp/data/remote/SpaceXApi.kt
@@ -1,12 +1,25 @@
 package com.example.spacexapp.data.remote
 
 import com.example.spacexapp.data.remote.dto.LaunchDto
+import com.example.spacexapp.data.remote.dto.LaunchpadDto
+import com.example.spacexapp.data.remote.dto.RocketDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface SpaceXApi {
 
-    @GET()
+    @GET("v4/launches")
     suspend fun getAllLaunches(): List<LaunchDto>
+
+    @GET("v4/rockets/{id}")
+    suspend fun getRocket(
+        @Path("id") rocketId: String
+    ): RocketDto
+
+    @GET("v4/launchpads/{id}")
+    suspend fun getLaunchpad(
+        @Path("id") launchpadId: String
+    ): LaunchpadDto
 
     companion object {
         const val BASE_URL = "https://api.spacexdata.com/"
