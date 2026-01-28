f52a6601b6e2fc212fefbe68c541cb174368eb04
JimenaD-creator <a01403295@tec.mx>
2026-01-27 23:32:45 -0600

    feat: create launches table to store their information

--- app/src/main/java/com/example/spacexapp/data/local/entity/LaunchEntity.kt
+++ app/src/main/java/com/example/spacexapp/data/local/entity/LaunchEntity.kt
@@ -1,4 +1,17 @@
 package com.example.spacexapp.data.local.entity
 
-class LaunchEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "launches")
+
+data class LaunchEntity (
+    @PrimaryKey val id: String,
+    val flightNumber: Int,
+    val missionName: String,
+    val rocketName: String,
+    val rocketType: String,
+    val launchSite: String,
+    val details: String?,
+    val launchDateUtc: String
+)
