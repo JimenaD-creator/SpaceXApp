b3f9b29e1ac8444d0cea9287ae47bd8fda50eed4
JimenaD-creator <a01403295@tec.mx>
2026-01-27 23:26:54 -0600

    feat: add launchpadId field to LaunchDto

--- app/src/main/java/com/example/spacexapp/data/remote/dto/LaunchDto.kt
+++ app/src/main/java/com/example/spacexapp/data/remote/dto/LaunchDto.kt
@@ -1,4 +1,16 @@
 package com.example.spacexapp.data.remote.dto
 
-class LaunchDto {
-}
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+
+data class LaunchDto (
+    @SerialName("id") val id: String,
+    @SerialName("flight_number") val flightNumber: Int,
+    @SerialName("name") val missionName: String,
+    @SerialName("rocket") val rocketId: String,
+    @SerialName("launchpad") val launchpadId: String?,
+    @SerialName("details") val details: String?,
+    @SerialName("date_utc") val dateUtc: String
+)
