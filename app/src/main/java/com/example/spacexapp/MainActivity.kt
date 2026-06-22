871650023f0a036750b6f433ea4a0fec792a65b6
JimenaDiazFranco <a01403295@tec.mx>
2026-06-22 17:36:30 -0600

    feat: integrate NavGraph and theme into MainActivity

--- app/src/main/java/com/example/spacexapp/MainActivity.kt
+++ app/src/main/java/com/example/spacexapp/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.spacexapp.ui.theme.SpaceXAppTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import dagger.hilt.android.AndroidEntryPoint
+import com.example.spacexapp.ui.theme.SpaceXAppTheme
+import com.example.spacexapp.ui.SpaceXNavGraph
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,29 +16,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpaceXAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SpaceXNavGraph()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SpaceXAppTheme {
-        Greeting("Android")
     }
 }
