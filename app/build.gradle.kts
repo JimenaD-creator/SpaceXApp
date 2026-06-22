f9d0ffcb94934a742d4252434285d93dc2c88840
JimenaDiazFranco <a01403295@tec.mx>
2026-06-22 17:38:43 -0600

    chore: add Accompanist SwipeRefresh dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.34.0")
 
     // Compose navigation
     implementation("androidx.navigation:navigation-compose:2.5.3")
